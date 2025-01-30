967a739d4563ed5e8fbb2fb43ad190ebbb2b81c8
tushar <tushar456kumar@gmail.com>
2025-01-30 14:47:15 +0530

    add some files

---
+++ webpack.prod.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
+  },
+  plugins: [new CleanWebpackPlugin()],
+};
